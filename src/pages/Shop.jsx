cc56d908242f8ff9d027f1b00775bf1db8c1f183
Brian <mulindijrn@gmail.com>
2024-08-30 17:07:21 +0300

    Add Shop component to display products in a grid
    Imported ProductsCard and productsData.
    Rendered a grid layout with products using data from productsData.
    Passed product properties: img, brand, name, price, and rating to the ProductsCard component.

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,6 +1,22 @@
+import ProductsCard from "../components/ProductCard"
+import { productsData } from "../constants"
+
+
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <div className="mt-16 mx-10 mb-2 grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-1 gap-5">
+      {productsData.map((product) => (
+        <ProductsCard 
+          key={product.name}
+          img = {product.imgURL}
+          brand = {product.brand}
+          name = {product.name}
+          price = {product.price} 
+          rating={product.rating}       
+        />
+      ))}
+  </div>
+
   )
 }
 
