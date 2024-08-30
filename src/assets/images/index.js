506fd66a3c7c03448bac1071bf34c0f2e1daced8
Brian <mulindijrn@gmail.com>
2024-08-30 03:38:15 +0300

    first commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,56 @@
+import alldocube from './Alldocube.jpg'
+import amtec from './Amtec.jpg'
+import amtec1 from './Amtec1.jpg'
+import macbook from './apple-macbook.jpg'
+import audio from './Audio.jpg'
+import dellInspiron from './dell-inspiron.jpg'
+import infinixHot30 from './Infinix-Hot-30i.jpg'
+import laptop1 from './laptop.jpg'
+import lenovo from './lenovo.jpg'
+import m1Bluetooth from './M1-Bluetooth.jpg'
+import technoCamon20 from './Techno-camon20.jpg'
+import technoSpark10 from './Techno-spark10.jpg'
+import xiomiRedmi from './Xiomi-Redmi.jpg'
+import television1 from './Television.jpg'
+import tornado from './Tornado.jpg'
+import vitron1 from './Vitron.jpg'
+import vitron2 from './Vitron-HTC3200.jpg'
+import vitron3 from './Vitron-HTC3218.jpg'
+import heroBackground from './hero-background.jpg'
+import camonHeroBackground from './Camon-hero-background.jpg'
+import dellHeroBackground from './Dell-hero-background.jpg'
+import infinixHeroBackground from './Infinix-hero-background.jpg'
+import infinixHeroBackground1 from './Infinix-hero-background1.jpg'
+import LenovoHeroBackground from './Lenovo-hero-background.png'
+import skyworthHeroBackground from './Skyworth-hero-background.jpg'
+import spark20HeroBackground from './Spark20-hero-background.jpg'
+
+
+export{
+    alldocube,
+    amtec,
+    amtec1,
+    macbook,
+    audio,
+    dellInspiron,
+    infinixHot30,
+    laptop1,
+    lenovo,
+    m1Bluetooth,
+    technoCamon20,
+    technoSpark10,
+    xiomiRedmi,
+    television1,
+    tornado,
+    vitron1,
+    vitron2,
+    vitron3,
+    heroBackground,
+    camonHeroBackground,
+    dellHeroBackground,
+    infinixHeroBackground,
+    infinixHeroBackground1,
+    LenovoHeroBackground,
+    skyworthHeroBackground,
+    spark20HeroBackground
+}
