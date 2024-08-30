cddcb52197e85bc4c4d2d00be3bafbfdb7a5c7c5
Brian <mulindijrn@gmail.com>
2024-08-30 16:16:47 +0300

    Export page components for use in routing
    - Imported and exported page components: Home, Shop, About, Contact, Cart, Faqs, Login, and SignUp

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from './Home';
+import Shop from './Shop';
+import About from './About';
+import Contact from './Contacts';
+import Cart from './Cart';
+import Faqs from './Faqs';
+import Login from './Login';
+import SignUp from './Signup';
+
+export {    
+    Home,
+    Shop,
+    About,
+    Contact,
+    Cart,
+    Faqs,
+    Login,
+    SignUp
+}
