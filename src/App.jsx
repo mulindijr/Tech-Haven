aca33e38f56995a798665a8db56b0a3f9f7c0455
Brian <mulindijrn@gmail.com>
2024-09-05 17:26:21 +0300

    Update the Routes in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Home, Shop, About, Contact, Cart, Faqs, Login, SignUp } from './pages';
 import Navigation from './components/Navigation'; 
 import Footer from './components/Footer'
 
 const App = () => {
-  return (
-    <Router>
-      <div>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/about-us" element={<About />} />
-          <Route path="/contact-us" element={<Contact />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/faqs" element={<Faqs />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+  return (    
+    <div>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/contact-us" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/faqs" element={<Faqs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <Footer />
+    </div>   
   );
 };
 
