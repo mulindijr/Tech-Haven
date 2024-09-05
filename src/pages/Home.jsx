a435aa9933c2e58e90daa95c9e23cc0c01c6494f
Brian <mulindijrn@gmail.com>
2024-09-06 02:13:20 +0300

    import BestSellers component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from '../components/Hero'
 import FeaturedProducts from '../components/FeaturedProducts'
+import BestSellers from '../components/BestSellers'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <FeaturedProducts />
+      <BestSellers />
     </div>
   )
 }
