4bcc83deb99b37f3621038aa70341ef4f6e499a2
Brian <mulindijrn@gmail.com>
2024-09-05 15:33:14 +0300

    Add user profile dropdown menu with hover effect and search icon

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -26,6 +26,22 @@ const Navigation = () => {
                 </NavLink>
               ))}
             </ul>
+
+            <div className='flex items-center gap-6'>
+              <FaSearch className='w-5 cursor-pointer'/>
+
+              <div className='group relative'>
+                <FaUser className='w-5 cursor-pointer'/>
+                <div className='group-hover:block hidden absolute dropdown-menu right-0 p-4'>
+                  <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                    <p className='cursor-pointer hover:text-black'>My Profile</p>
+                    <p className='cursor-pointer hover:text-black'>Orders</p>
+                    <p className='cursor-pointer hover:text-black'>Logout</p>
+                  </div>
+                </div>
+              </div>   
+                        
+            </div>
           </nav>
         </header>
     );
