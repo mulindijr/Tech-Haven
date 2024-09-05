66c8cb896d9b3449de68d60c6c1ef82edba8b9a2
Brian <mulindijrn@gmail.com>
2024-09-05 23:08:48 +0300

    Implement hero section with Slick slider for background images and autoplay feature

---
+++ src/components/Hero.jsx
@@ -0,0 +1,35 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import { infinixHeroBackground, infinixHeroBackground1, camonHeroBackground } from "../assets/images";
+
+const Hero = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    };
+
+  return (
+    <div className="hero-slider">
+      <Slider {...settings}>
+        <div>
+          <img src={infinixHeroBackground} alt="Spark 20" className="object-cover w-full"/>
+        </div>
+        <div>
+          <img src={infinixHeroBackground1} alt="Infinix Hero" className="object-cover w-full"/>
+        </div>
+        <div>
+          <img src={camonHeroBackground} alt="Lenovo Hero" className="object-cover w-full"/>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Hero;
