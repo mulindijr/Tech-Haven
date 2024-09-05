61c1f28490d07e006df2b6c5af4b289298cc1ddd
Brian <mulindijrn@gmail.com>
2024-09-06 01:23:03 +0300

    Update ProductItem component: - Added currency context usage for price display - Applied hover scaling effect to image - Improved styling and layout for product details

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,24 +1,32 @@
+import { useContext } from "react";
 import { renderStars } from "./utils";
+import { ShopContext } from "../context/ShopContext";
+import { Link } from "react-router-dom";
 
-const ProductsCard = ({img, brand, name, price, rating}) => {
+const ProductItem = ({id, img, brand, name, price, rating}) => {
+
+  const {currency} =useContext(ShopContext)
+
     return (
-      <div className="flex flex-1 flex-col w-full max-sm:w-full shadow-md p-5 cursor-pointer">
-        <div className="hover:scale-105 hover:duration-500 duration-500">
-          <div className="flex justify-center items-center">
-            <img 
-              src={img} alt={name}
-              className="w-[200px] h-[200px]"
-            />
-          </div>
-          <span className="text-sm text-green-400">{brand}</span>
-          <h3 className="mt-2 text-lg leading-normal font-semibold line-clamp-2">{name}</h3>
-          <div>
-            {renderStars(rating)}
-          </div>
-          <p className="text-pink-400">{price}</p>            
-        </div>      
+      <Link 
+        className="shadow-md p-5 cursor-pointer group"
+        to={`/product/${id}`}
+      >
+      <div className="">
+        <div className="flex justify-center items-center">
+          <img
+            src={img}
+            alt={name}
+            className="transform transition-transform duration-500 group-hover:scale-105"
+          />
+        </div>
+        <span className="text-sm text-green-400">{brand}</span>
+        <p className="mt-2 text-sm leading-normal font-semibold line-clamp-2">{name}</p>
+        <div>{renderStars(rating)}</div>
+        <p className="text-pink-400">{currency}{price}</p>
       </div>
+    </Link>
     )
   }
   
-  export default ProductsCard;
+  export default ProductItem;
