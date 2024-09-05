f638946b37f4f68ff00c1863858809a6db378f49
Brian <mulindijrn@gmail.com>
2024-09-05 23:10:33 +0300

    Add reusable Title component with dynamic text and styling

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+const Title = ({title1, title2}) => {
+
+  return (
+    <div className="inline-flex gap-2 items-center mb-3">
+        <p className="text-3xl text-gray-500 text-center">{title1}</p>
+        <span className="text-3xl text-red-400 text-center">{title2}</span>
+    </div>
+  )
+}
+
+export default Title
