64aaa1671e76c7b622c8af6f89c63d967ef7c130
Brian <mulindijrn@gmail.com>
2024-09-05 22:29:26 +0300

    Wrap App component with ShopContextProvider for global state management

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  <ShopContextProvider> 
     <App />
+  </ShopContextProvider>
   </BrowserRouter>,
 )
