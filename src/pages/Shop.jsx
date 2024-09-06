f020f791c13096151a6da542f48ae67d8e4df45b
Brian <mulindijrn@gmail.com>
2024-09-06 19:00:17 +0300

    Add product sorting functionality and apply useEffect to update sorting
    - Implemented sortProduct function to sort filtered products by price.
    - Introduced sorting logic for 'Low-High' and 'High-Low' based on the selected sortType.
    - Applied useEffect to automatically sort products whenever sortType changes.
    - Ensured the default case re-applies filters if no sorting option is selected.

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -10,6 +10,7 @@ const Shop = () => {
   const [filterProducts, setFilterProducts] = useState([])
   const [category, setCategory] = useState([])
   const [brand, setBrand] = useState([])
+  const [sortType, setSortType] = useState('relevant')
 
   const toggleCategory = (e) => {
     if(category.includes(e.target.value)){
@@ -40,9 +41,32 @@ const Shop = () => {
     setFilterProducts(productsCopy)
   }
 
+  const sortProduct =() => {
+
+    let filterProductsCopy = filterProducts.slice()
+    
+    switch(sortType){
+      case 'Low-High':
+        setFilterProducts(filterProductsCopy.sort((a,b) => a.price - b.price))
+        break;
+
+      case 'High-Low':
+        setFilterProducts(filterProductsCopy.sort((a,b) => b.price - a.price))
+        break;
+
+      default:
+        applyFilter();
+        break;
+    }
+  }
+
   useEffect(() => {
     applyFilter();
   },[category, brand])
+
+  useEffect(() => { 
+    sortProduct();
+  } ,[sortType])
 
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
@@ -109,10 +133,10 @@ const Shop = () => {
         <div className="flex justify-between text-base sm:text-2xl mb-4">
           <Title title1={'ALL'} title2={'PRODUCTS'}/>
           {/*sort by*/}
-          <select className="border-2 border-gray-300 text-sm px-2">
+          <select onChange={(e) => setSortType(e.target.value)} className="border-2 border-gray-300 text-sm px-2">
             <option value="Sort by: Relevance">Sort by: Relavance</option>
-            <option value="Price: Low to High">Price: Low to High</option>
-            <option value="Price: High to Low">Price: High to Low</option>
+            <option value="Low-High">Price: Low to High</option>
+            <option value="High-Low">Price: High to Low</option>
           </select>
         </div>
         {/*Map Products*/}
