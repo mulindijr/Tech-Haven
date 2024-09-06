7ca8f06ff068eb6cd137d111c052fa8c2385b07f
Brian <mulindijrn@gmail.com>
2024-09-06 19:42:38 +0300

    Import SearchBar and product component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home, Shop, About, Contact, Cart, Faqs, Login, SignUp } from './pages';
+import { Home, Shop, About, Contact, Cart, Faqs, Login, SignUp, Product } from './pages';
 import Navigation from './components/Navigation'; 
 import Footer from './components/Footer'
+import SearchBar from './components/SearchBar';
 
 const App = () => {
   return (    
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <Navigation />
+      <SearchBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/product/:productId" element={<Product />} />
         <Route path="/about-us" element={<About />} />
         <Route path="/contact-us" element={<Contact />} />
         <Route path="/cart" element={<Cart />} />
