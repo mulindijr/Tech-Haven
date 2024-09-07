58d6ba394cf44fc8d199d04efa7d32c966ac3aa2
Brian <mulindijrn@gmail.com>
2024-09-08 00:47:51 +0300

    Update Product component to display product details including image, brand, name, description, price, and rating stars

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,11 +1,12 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { ShopContext } from "../context/ShopContext";
+import { renderStars } from "../components/utils";
 
 const Product = () => {
 
   const { productId} = useParams();
-  const {productsData} = useContext(ShopContext);
+  const {productsData, currency} = useContext(ShopContext);
   const [productData, setProductData] = useState([]);
 
   const fetchProductsData = async () => {
@@ -13,7 +14,6 @@ const Product = () => {
     productsData.map(item => {
       if(item._id === productId) {
         setProductData(item)
-        console.log(item)
         return null;
       }
     })
@@ -24,9 +24,21 @@ const Product = () => {
     fetchProductsData()
   }, [productId])
 
-  return (
-    <div>Product</div>
-  )
+  return productData ? (
+    <div className="border-t pt-10 transition-opacity eas duration-500 opacity-100">
+      <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+        <div>
+          <img src={productData.imgURL} alt={productData.name} />
+          <p>{productData.brand}</p>
+          <p>{productData.name}</p>
+          <p>{productData.description}</p>
+          <p>{currency}{productData.price}</p>
+          <p>{renderStars(productData.rating)}</p>
+
+        </div>
+      </div>
+    </div>
+  ): <div className="opacity-0"></div>
 }
 
 export default Product
