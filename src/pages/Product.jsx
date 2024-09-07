582b1546ab26aafea1b8aaa0a13b455bec78a41d
Brian <mulindijrn@gmail.com>
2024-09-08 02:08:57 +0300

    style product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -25,16 +25,31 @@ const Product = () => {
   }, [productId])
 
   return productData ? (
-    <div className="border-t pt-10 transition-opacity eas duration-500 opacity-100">
-      <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
-        <div>
-          <img src={productData.imgURL} alt={productData.name} />
-          <p>{productData.brand}</p>
-          <p>{productData.name}</p>
-          <p>{productData.description}</p>
-          <p>{currency}{productData.price}</p>
-          <p>{renderStars(productData.rating)}</p>
-
+    <div className="border-t pt-10 mb-10 transition-opacity eas duration-500 opacity-100">      
+      <div className="flex gap-5 sm:gap-5 flex-col sm:flex-row">
+        <div className="w-full sm:w-1/3 flex items-center justify-center">
+          <img 
+            className="sm:w-full h-auto object-cover"
+            src={productData.imgURL} 
+            alt={productData.name} 
+          /> 
+        </div>
+        <div className="w-full sm:w-1/2">
+          <p className="font-medium sm:text-2xl mt-2">{productData.name}</p>
+          <p className="font-medium text-sm text-green-500 mt-2">{productData.brand}</p>
+          <hr className="mt-4 sm:w-5/5"/>
+          <p className="font-medium sm:text-2xl mt-2">{currency} {productData.price}</p>
+          <div className="flex items-center gap-2 mt-2">
+            <span className="font-medium text-sm">{renderStars(productData.rating)}</span>
+            <p className="italic">(66 verified ratings)</p>
+          </div>
+          <button className="bg-yellow-500 text-white font-bold px-8 py-1 sm:py-3 mt-2 w-full sm:text-md cursor-pointer active:bg-gray-700">ADD TO CART</button>
+          <hr className="mt-4 sm:w-5/5"/>
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% original product</p>
+            <p>Cash on delivery is available on  this product</p>
+            <p>Easy return and exchange policy within 7 days</p>
+          </div>      
         </div>
       </div>
     </div>
