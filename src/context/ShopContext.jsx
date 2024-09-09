2d01816e71c6001b6b54b18d0320e9121c94c1c4
Brian <mulindijrn@gmail.com>
2024-09-09 23:57:31 +0300

    Add addToCart function to correctly handle item addition and state updates

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { productsData } from "../constants";
 
 
@@ -9,10 +9,35 @@ const ShopContextProvider = ({children}) => {
     const delivery_fee = 200
     const[search, setSearch] = useState('')
     const[showSearch, setShowSearch] = useState(false)
+    const[cartItems, setCartItems] = useState({})
+
+    const addToCart = (itemId) => {
+
+        const cartData = structuredClone(cartItems);
+
+        if (cartData[itemId]) {
+            if (cartData[itemId]){
+                cartData[itemId] += 1;
+            }
+            else{
+                cartData[itemId] = 1;
+            }
+        }
+        else{
+            cartData[itemId] = {};
+            cartData[itemId] = 1;
+        }
+        setCartItems(cartData);
+    }
+    
+    useEffect(() => {
+        console.log(cartItems)
+    }, [cartItems])
 
     const value ={
         productsData, currency, delivery_fee,  
-        search, setSearch, showSearch, setShowSearch 
+        search, setSearch, showSearch, setShowSearch,
+        cartItems, addToCart
 
     }
     
