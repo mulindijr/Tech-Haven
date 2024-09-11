42c54b60a787aaecaa7aeaa7dc345f171a4f8c47
Brian <mulindijrn@gmail.com>
2024-09-11 17:09:08 +0300

    Add cart item display and quantity input to Cart component
    - Implemented UI for displaying cart items with product details.
    - Added quantity input for each cart item, allowing user adjustments.
    - Utilized Tailwind CSS for styling and responsive design.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,9 @@
 import { useContext, useState, useEffect } from "react";
 import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
 
 const Cart = () => {
-  const { productsData, cartItems } = useContext(ShopContext);
+  const { productsData, currency, cartItems } = useContext(ShopContext);
   const [cartData, setCartData] = useState([]);
 
   useEffect(() => {
@@ -26,6 +27,29 @@ const Cart = () => {
 
   return (
     <div className=" border-t pt-14">
+
+      <div className="text-2xl mb-3">
+        <Title title1={'Your'} title2={'Cart'}/> 
+      </div>
+
+      <div>
+        {
+          cartData.map((item,index)=>(
+            <div key={index} className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4">
+              <div className="flex items-start gap-6">
+                <img src={item.imgURL} alt={item.name} className="w-16 sm:w-20"/>
+                <div>
+                  <p className="text-xs sm:text-lg font-medium">{item.name}</p>
+                  <div className="mt-2">
+                    <p>{currency} {item.price}</p>
+                  </div>
+                </div>
+              </div>
+              <input className="border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1" type="number" min={1} defaultValue={item.quantity} />
+            </div>
+          ))
+        }
+      </div>
       
     </div>
   );
