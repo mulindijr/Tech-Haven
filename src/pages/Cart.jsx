579407e954864ca9eb417e38f3452d303e51baf9
Brian <mulindijrn@gmail.com>
2024-09-12 02:41:05 +0300

    Add button click handler to navigate to '/place-order' route.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,7 +5,7 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 import CartTotal from "../components/CartTotal";
 
 const Cart = () => {
-  const { productsData, currency, cartItems, updateQuantity } = useContext(ShopContext);
+  const { productsData, currency, cartItems, updateQuantity,navigate } = useContext(ShopContext);
   const [cartData, setCartData] = useState([]);
 
   useEffect(() => {
@@ -57,6 +57,11 @@ const Cart = () => {
       <div className="flex justify-end my-20">
         <div className="w-full sm:w-[450px]">
           <CartTotal />
+          <div className="w-full text-end">
+            <button onClick={() => navigate('/place-order')} className="bg-black text-white text-sm my-8 px-8 py-3">
+              PROCEED TO CHECKOUT
+            </button>
+          </div>
         </div>
       </div>
       
