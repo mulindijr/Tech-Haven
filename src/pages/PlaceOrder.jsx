36a4542443f7b9669d5243eafc7838b5c95034e3
Brian <mulindijrn@gmail.com>
2024-09-12 02:53:51 +0300

    Add PlaceOrder component with delivery information section structure.

---
+++ src/pages/PlaceOrder.jsx
@@ -0,0 +1,17 @@
+import Title from "../components/Title"
+
+const PlaceOrder = () => {
+  return (
+    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
+        {/* Left */}
+        <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
+            <div className="text-xl sm:text-2xl my-3">
+                <Title title1={'DELIVERY'} title2={'INFORMATION'}/>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default PlaceOrder
