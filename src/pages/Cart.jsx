b586fa449f5cf3c119aae2d4913420af4f8955aa
Brian <mulindijrn@gmail.com>
2024-09-11 17:06:56 +0300

    Implement Cart component logic: Fetch and display cart items with product details
    - Added useEffect to process cartItems and productsData.
    - Converted cartItems into an array of product objects with quantities.
    - Updated cartData state to reflect processed cart items.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,34 @@
-const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+import { useContext, useState, useEffect } from "react";
+import { ShopContext } from "../context/ShopContext";
 
-export default Cart
+const Cart = () => {
+  const { productsData, cartItems } = useContext(ShopContext);
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+    const tempData = [];
+
+    for (const itemId in cartItems) {
+      const quantity = cartItems[itemId];
+      if (quantity > 0) {
+        const product = productsData.find((product) => product._id === itemId);
+        if (product) {
+          tempData.push({
+            ...product,
+            quantity
+          });
+        }
+      }
+    }
+    
+    setCartData(tempData);
+  }, [cartItems, productsData]);
+
+  return (
+    <div className=" border-t pt-14">
+      
+    </div>
+  );
+};
+
+export default Cart; 
