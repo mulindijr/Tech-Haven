fe56a1b3ccef5980d4ce199152d66caa7df9e3da
Brian <mulindijrn@gmail.com>
2024-09-12 03:57:18 +0300

    Add payment method selection section to PlaceOrder page with options for M-PESA, PAYPAL, STRIPE, and CASH ON DELIVERY.

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,7 +1,11 @@
+import React, { useState } from 'react'
 import Title from "../components/Title"
 import CartTotal from "../components/CartTotal"
 
 const PlaceOrder = () => {
+
+    const [method, setMethod] = useState('M-PESA');
+    
   return (
     <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
         {/* ------Left------ */}
@@ -36,7 +40,27 @@ const PlaceOrder = () => {
 
             <div className="mt-12"> 
                 <Title title1={'PAYMENT'} title2={'METHOD'} />
-            </div>         
+                
+                {/* -------Payment method Selection------- */}
+                <div className="flex gap-3 flex-col mb-5 lg:flex-row">
+                    <div onClick={() => setMethod('M-PESA')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
+                        <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'M-PESA' ? "bg-green-400" : ''}`}></p>
+                        <p className="text-gray-500 text-sm font-medium mx-2">M-PESA</p>
+                    </div>
+                    <div onClick={() => setMethod('PAYPAL')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
+                        <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'PAYPAL' ? "bg-green-400" : ''}`}></p>
+                        <p className="text-gray-500 text-sm font-medium mx-2">PAYPAL</p>
+                    </div>
+                    <div onClick={() => setMethod('STRIPE')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
+                        <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'STRIPE' ? "bg-green-400" : ''}`}></p>
+                        <p className="text-gray-500 text-sm font-medium mx-2">STRIPE</p>
+                    </div>
+                    <div onClick={() => setMethod('COD')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
+                        <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'COD' ? "bg-green-400" : ''}`}></p>
+                        <p className="text-gray-500 text-sm font-medium mx-2">CASH ON DELIVERY</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
