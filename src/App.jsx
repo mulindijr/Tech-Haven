82c7aa720118a52a8528dc9cb08fc165a9089d7b
Brian <mulindijrn@gmail.com>
2024-09-14 23:58:19 +0300

    Import Login Component and delete the login and signup imports from pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home, Shop, About, Contact, Cart, Faqs, Login, SignUp, Product, PlaceOrder, Orders } from './pages';
+import { Home, Shop, About, Contact, Cart, Faqs, Product, PlaceOrder, Orders } from './pages';
 import Navigation from './components/Navigation'; 
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar';
+import Login from './components/Login';
 
 const App = () => {
   return (    
@@ -18,7 +19,6 @@ const App = () => {
         <Route path="/cart" element={<Cart />} />
         <Route path="/faqs" element={<Faqs />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
         <Route path="/place-order" element={<PlaceOrder />} />
         <Route path="/orders" element={<Orders />} />
       </Routes>
