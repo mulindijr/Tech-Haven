7369ab7336f623536d9766d48ca4b7937a65c77f
Brian <mulindijrn@gmail.com>
2024-09-16 23:59:43 +0300

    Import Social Media Links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { footerLinks } from "../constants"
 import { Link } from 'react-router-dom';
+import SocialMediaLinks from "./SocialMediaLinks";
 
 const Footer = () => {
   return (
@@ -11,12 +12,7 @@ const Footer = () => {
                       <span className='text-red-500'>Haven</span>
                     </h1>
                     <p className="mt-6 text-base leading-7 text-white-400 sm:max-w-sm">Get electronic devices at your nearest Tech Haven store.Find your perfect device in store.Get Rewards</p>
-                    <div className=" flex items-center gap-5 mt-2">
-                        <div className="flex justify-center items-center w-5 h-5 rounded-full p-5 cursor-pointer bg-white">i</div>
-                        <div className="flex justify-center items-center w-5 h-5 rounded-full p-5 cursor-pointer bg-white">f</div>
-                        <div className="flex justify-center items-center w-5 h-5 rounded-full p-5 cursor-pointer bg-white">x</div>
-                        <div className="flex justify-center items-center w-5 h-5 rounded-full p-5 cursor-pointer bg-white">l</div>
-                    </div>
+                    <SocialMediaLinks />
                 </div>    
                 <div className="mt-5 flex flex-1 flex-wrap justify-between">
                     {footerLinks.map((section) => (
