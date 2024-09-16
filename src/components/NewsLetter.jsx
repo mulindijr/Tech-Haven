a56642f0f1060e58a04694fa3445247b82913079
Brian <mulindijrn@gmail.com>
2024-09-16 23:46:34 +0300

    Modify the sytling for subscribe button for small devices

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -10,7 +10,7 @@ const NewsLetter = () => {
         <p className="text-gray-400 mt-3">Sign up now to enjoy a 20% discount and stay updated with our latest offers and updates.</p>
         <form onSubmit={onSubmitHandler} className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3 rounded-full">
             <input className="w-full sm:flex-1 outline-none" type="email" placeholder="Enter your Email" required/>
-            <button type="submit" className="bg-black text-white text-lg px-10 py-4">Subscribe</button>
+            <button type="submit" className="bg-black text-white text-lg px-4 py-1 sm:px-10 sm:py-4">Subscribe</button>
         </form>
     </div>
   )
