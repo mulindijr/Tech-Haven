4104201f87a583e552552990c26bbb8a56a2785f
Brian <mulindijrn@gmail.com>
2024-09-18 01:07:06 +0300

    feat: Display 'Cart is empty' message when cart has no items
    - Added conditional rendering to show a message when the cart is empty.
    - Display cart items and checkout options when items are available.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -34,37 +34,44 @@ const Cart = () => {
         <Title title1={'Your'} title2={'Cart'}/> 
       </div>
 
-      <div>
-        {
-          cartData.map((item,index)=>(
-            <div key={index} className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4">
-              <div className="flex items-start gap-6">
-                <img src={item.imgURL} alt={item.name} className="w-16 sm:w-20"/>
-                <div>
-                  <p className="text-xs sm:text-lg font-medium">{item.name}</p>
-                  <div className="mt-2">
-                    <p>{currency} {item.price}</p>
-                  </div>
-                </div>
-              </div>
-              <input onChange={(e) => e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id, Number(e.target.value))} className="border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1" type="number" min={1} defaultValue={item.quantity} />
-              <RiDeleteBin6Line onClick={() => updateQuantity(item._id,0)} className="w-5 h-5 cursor-pointer"/>
-            </div>
-          ))
-        }
-      </div>
-
-      <div className="flex justify-end my-20">
-        <div className="w-full sm:w-[450px]">
-          <CartTotal />
-          <div className="w-full text-end">
-            <button onClick={() => navigate('/place-order')} className="bg-black text-white text-sm my-8 px-8 py-3">
-              PROCEED TO CHECKOUT
-            </button>
-          </div>
+      {cartData.length === 0 ? (
+        <div className="text-center text-gray-500">
+          Your cart is empty
         </div>
-      </div>
-      
+      ) : 
+        <>
+          <div>
+            {
+              cartData.map((item,index)=>(
+                <div key={index} className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4">
+                  <div className="flex items-start gap-6">
+                    <img src={item.imgURL} alt={item.name} className="w-16 sm:w-20"/>
+                    <div>
+                      <p className="text-xs sm:text-lg font-medium">{item.name}</p>
+                      <div className="mt-2">
+                        <p>{currency} {item.price}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <input onChange={(e) => e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id, Number(e.target.value))} className="border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1" type="number" min={1} defaultValue={item.quantity} />
+                  <RiDeleteBin6Line onClick={() => updateQuantity(item._id,0)} className="w-5 h-5 cursor-pointer"/>
+                </div>
+              ))
+            }
+          </div>
+
+          <div className="flex justify-end my-20">
+            <div className="w-full sm:w-[450px]">
+              <CartTotal />
+              <div className="w-full text-end">
+                <button onClick={() => navigate('/place-order')} className="bg-black text-white text-sm my-8 px-8 py-3">
+                  PROCEED TO CHECKOUT
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      }      
     </div>
   );
 };
