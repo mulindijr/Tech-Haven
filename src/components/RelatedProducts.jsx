256394b4aef0920b9b18f68efba6d222a8a47046
Brian <mulindijrn@gmail.com>
2024-09-18 02:51:05 +0300

    Add message for no related products found

--- src/components/RelatedProducts.jsx
+++ src/components/RelatedProducts.jsx
@@ -28,24 +28,30 @@ const RelatedProducts = ({ category, brand, currentProductId }) => {
   }; 
 
   return (
-    <div className="my-20">
+    <div className="my-10">
       <div>
         <Title title1={'Related'} title2={'Products'} />
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-        {related.map((item, index) => (
-          <ProductItem
-            key={index}
-            id={item._id}
-            img={item.imgURL}
-            brand={item.brand}
-            name={item.name}
-            price={item.price}
-            rating={item.rating}
-            onClick={() => handleProductClick(item._id)}
-          />
-        ))}
-      </div>
+      {related.length > 0 ? (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+          {related.map((item, index) => (
+            <ProductItem
+              key={index}
+              id={item._id}
+              img={item.imgURL}
+              brand={item.brand}
+              name={item.name}
+              price={item.price}
+              rating={item.rating}
+              onClick={() => handleProductClick(item._id)}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="bg-slate-200 p-10 text-center text-lg sm:text-3xl text-red-500 rounded-xl">
+          No related products are available.
+        </div>
+      )}
     </div>
   );
 };
