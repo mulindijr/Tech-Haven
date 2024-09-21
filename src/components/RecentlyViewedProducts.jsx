571de24990d304b642f2c45a6dc90d6118108f42
Brian <mulindijrn@gmail.com>
2024-09-21 23:48:42 +0300

    Implement RecentlyViewed component to display recently viewed products.

---
+++ src/components/RecentlyViewedProducts.jsx
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react';
+import { ShopContext } from "../context/ShopContext";
+import Title from './Title';
+import ProductItem from './ProductItem';
+
+const RecentlyViewed = () => {
+    const { recentlyViewed } = useContext(ShopContext);
+
+    return (
+        <div>
+            <div>
+                <Title title1={'Recently'} title2={'Viewed'}/>
+            </div>
+            {recentlyViewed.length > 0 ? (
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mb-6">
+                    {recentlyViewed.map((item, index) => (
+                        <ProductItem 
+                            key={index}
+                            id={item._id}
+                            img={item.imgURL}
+                            brand={item.brand}
+                            name={item.name}
+                            price={item.price}
+                            rating={item.rating}                    
+                        />
+                    ))}
+                </div>
+            ) : (
+                <div className="bg-slate-200 p-10 text-center text-lg sm:text-3xl text-red-500 rounded-xl mb-6">
+                     No recenlty viewed products
+                </div>
+            )}
+        </div>
+        
+    );
+};
+
+export default RecentlyViewed;
