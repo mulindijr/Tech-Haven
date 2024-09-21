2cfe45b1b3fdfa02587cc51c67d0987308b684a1
Brian <mulindijrn@gmail.com>
2024-09-22 01:37:42 +0300

    Refactor: Add wrapper div to isolate padding styles from the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,24 @@ import Login from './components/Login';
 
 const App = () => {
   return (    
-    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+    <div>
       <Navigation />
-      <SearchBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/product/:productId" element={<Product />} />
-        <Route path="/about-us" element={<About />} />
-        <Route path="/contact-us" element={<Contact />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/faqs" element={<Faqs />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/place-order" element={<PlaceOrder />} />
-        <Route path="/orders" element={<Orders />} />
-      </Routes>
-      <Footer />
+      <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>      
+        <SearchBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/about-us" element={<About />} />
+          <Route path="/contact-us" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/faqs" element={<Faqs />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/place-order" element={<PlaceOrder />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+        <Footer />
+      </div>
     </div>   
   );
 };
