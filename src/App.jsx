748608177978811a8a1d07faacfa3ffd23600db7
Brian <mulindijrn@gmail.com>
2024-09-23 14:12:09 +0300

    Implement conditional rendering for Login component based on showLogin state

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,32 @@ import Navigation from './components/Navigation';
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar';
 import Login from './components/Login';
+import { useState } from 'react';
 
 const App = () => {
-  return (    
-    <div>
-      <Navigation />
-      <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>      
-        <SearchBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/product/:productId" element={<Product />} />
-          <Route path="/about-us" element={<About />} />
-          <Route path="/contact-us" element={<Contact />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/faqs" element={<Faqs />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/place-order" element={<PlaceOrder />} />
-          <Route path="/orders" element={<Orders />} />
-        </Routes>
-        <Footer />
-      </div>
-    </div>   
+  const [showLogin, setShowLogin] = useState(false)
+  return (  
+    <>
+      {showLogin ? <Login setShowLogin = {setShowLogin}/> : <></>}  
+      <div>
+        <Navigation setShowLogin = {setShowLogin}/>
+        <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>      
+          <SearchBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/product/:productId" element={<Product />} />
+            <Route path="/about-us" element={<About />} />
+            <Route path="/contact-us" element={<Contact />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/faqs" element={<Faqs />} />
+            <Route path="/place-order" element={<PlaceOrder />} />
+            <Route path="/orders" element={<Orders />} />
+          </Routes>
+          <Footer />
+        </div>
+      </div> 
+    </>  
   );
 };
 
