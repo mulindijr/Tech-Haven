02ac036ba1599737f68f6f7c221840c8cf22f515
Brian <mulindijrn@gmail.com>
2024-09-23 14:13:49 +0300

    Add close button functionality to login modal using MdClose icon

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
+import { MdClose } from "react-icons/md";
 
-
-const Login = () => {
+const Login = ({setShowLogin}) => {
 
   const [currentState, setCurrentState] = useState('Login')
 
@@ -11,13 +11,14 @@ const Login = () => {
 
   return (
     <form onSubmit={onSubmitHandler} className="flex flex-col w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800 bg-slate-200 px-5 py-5 rounded mb-10">
-      <div className="inline-flex items-center gap-2 mb-2">
-        <h2 className="prata-regular text-3xl">{currentState}</h2>
+      <div className="flex items-center justify-between mb-2">
+        <h2 className="text-3xl">{currentState}</h2>
+        <MdClose onClick={() => setShowLogin(false)} className="w-8 h-8 cursor-pointer"/>
       </div>      
-        {currentState === "Login" ? <></>: <input type="text" placeholder="Fullname" required className="w-full px-3 py-2 border border-gray-400 outline-none rounded"/>}
-        <input type="email" placeholder="Email" required className="w-full px-3 py-2 border border-gray-400 outline-none rounded"/>
-        <input type="password" placeholder="Password" required className="w-full px-3 py-2 border border-gray-400 outline-none rounded"/>
-        <button className="w-full py-2 text-xl border-none rounded text-white bg-red-500">{currentState === 'Sign Up' ? "Create Account" : "Login"}</button>
+      {currentState === "Login" ? <></>: <input type="text" placeholder="Fullname" required className="w-full px-3 py-2 border border-gray-400 outline-none rounded"/>}
+      <input type="email" placeholder="Email" required className="w-full px-3 py-2 border border-gray-400 outline-none rounded"/>
+      <input type="password" placeholder="Password" required className="w-full px-3 py-2 border border-gray-400 outline-none rounded"/>
+      <button className="w-full py-2 text-xl border-none rounded text-white bg-red-500">{currentState === 'Sign Up' ? "Create Account" : "Login"}</button>
       
       {currentState === 'Sign Up' 
       ? <div className="flex gap-2">
