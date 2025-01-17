f7af6aa0a4475b25e91889f0406090c6dbb6c71f
Brian <mulindijrn@gmail.com>
2025-01-18 02:11:19 +0300

    Create product controller with functions for adding, listing, removing, and retrieving a single product

---
+++ backend/controllers/productController.js
@@ -0,0 +1,26 @@
+
+
+// Function for adding a new product
+const addProduct = async (req, res) => {
+
+}
+
+
+// Function for getting all products
+const listProducts = async (req, res) => {
+
+}
+
+
+// Function for removing a product
+const removeProduct = async (req, res) => {
+
+}
+
+
+// Function for getting a single product
+const singleProduct = async (req, res) => {
+
+}
+
+export { addProduct, listProducts, removeProduct, singleProduct };
