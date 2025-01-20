aacd3cf0baf35fa579dc67bae48d6483885cd046
Brian <mulindijrn@gmail.com>
2025-01-20 22:15:31 +0300

    Delete ID field and add date field

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
   imgURL: { type: String, required: true },
   brand: { type: String, required: true },
   name: { type: String, required: true },
@@ -9,6 +8,7 @@ const productSchema = new mongoose.Schema({
   category: { type: String, required: true },
   description: { type: String, required: true },
   rating: { type: Number, required: true },
+  date:{type: Number, required: true}
 });
 
 const productModel = mongoose.models.Product || mongoose.model("Product", productSchema);
