f8ca50e48b683e58b0c97d967cb406be7685a46d
Brian <mulindijrn@gmail.com>
2025-01-29 13:19:59 +0300

    Structured the App with a Navbar, Sidebar, and a responsive layout. Added a horizontal divider below the Navbar.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,17 @@
+import Navbar from "./components/Navbar"
+import Sidebar from "./components/Sidebar"
 function App() {
 
   return (
-    <>
-    
-      
-    </>
+    <div className="bg-gray-50 min-h-screen">
+      <>
+        <Navbar />
+        <hr />
+        <div className="flex w-full">
+          <Sidebar />
+        </div>      
+      </>
+    </div>
   )
 }
 
