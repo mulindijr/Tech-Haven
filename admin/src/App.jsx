bf9fa3031be5f3a765d42cd10068b9e5396ac284
Brian <mulindijrn@gmail.com>
2025-01-29 13:54:36 +0300

    Define routes for 'Add', 'List', and 'Orders' pages in the application using React Router.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,10 @@
+import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Sidebar from "./components/Sidebar"
+import Add from "./pages/Add"
+import List from "./pages/List"
+import Orders from "./pages/Orders"
+
 function App() {
 
   return (
@@ -9,6 +14,13 @@ function App() {
         <hr />
         <div className="flex w-full">
           <Sidebar />
+          <div className="w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base">
+            <Routes>
+              <Route path="/add" element={<Add />} />
+              <Route path="/list" element={<List />} />
+              <Route path="/orders" element={<Orders />} />
+            </Routes>
+          </div>
         </div>      
       </>
     </div>
