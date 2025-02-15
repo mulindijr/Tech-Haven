7813bb21e43ec5a75b1514b73dfd6eb1e19b1d6d
Brian <mulindijrn@gmail.com>
2025-02-15 15:00:48 +0300

    feat: add logout functionality in Navbar by clearing token

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = ({setToken}) => {
   return (
     <div className='flex justify-between items-center px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] py-4'>
         <h1 className='text-xl font-bold cursor-pointer'>
             Tech<span className='text-red-500'>Haven</span>
         </h1>
 
-        <button className='bg-red-400 text-white px-4 py-2 rounded-full'>
+        <button onClick={() => setToken('')} className='bg-red-400 text-white px-4 py-2 rounded-full'>
             Logout
         </button>
     </div>
