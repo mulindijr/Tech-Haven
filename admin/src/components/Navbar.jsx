e6407cfd7bfd9cc7711b55219a19fea535e3338a
Brian <mulindijrn@gmail.com>
2025-02-19 16:55:13 +0300

    Make the navbar sticky

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = ({setToken}) => {
   return (
-    <div className='flex justify-between items-center px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] py-4'>
+    <div className='flex justify-between items-center px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] py-4 sticky top-0 z-50 bg-gray-50 shadow-md'>
         <h1 className='text-xl font-bold cursor-pointer'>
             Tech<span className='text-red-500'>Haven</span>
         </h1>
