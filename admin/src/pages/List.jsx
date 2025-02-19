3fd2bdc6d881eb55e1cf87ee070cf06ef7d00c56
Brian <mulindijrn@gmail.com>
2025-02-19 16:27:47 +0300

    Refactor styling for responsiveness and add brand to the product list

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -46,28 +46,30 @@ const List = ({token}) => {
     <>
       <p className='mb-2'>All Products List</p>
       <div className='flex flex-col gap-2'>
-        <div className='grid md:grid-cols-[1fr_3fr_1fr_1fr_1fr] bg-gray-200 py-2 px-3 gap-2'>
+        <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr_1fr] items-center border border-red-500 bg-gray-200 py-2 px-3'>
           <b>Image</b>
           <b>Name</b>
+          <b>Brand</b>
           <b>Price</b>
           <b>Rating</b>
           <b className='text-center'>Action</b>
         </div>
         {
           list.map((item, index) => (                          
-            <div key={index} className='grid md:grid-cols-[1fr_3fr_1fr_1fr_1fr] py-2 px-3 gap-2'>
-              <img src = {item.imgURL} alt = {item.name} className='w-12 h-12 object-cover' />
-              <p>{item.name}</p>
+            <div key={index} className='flex flex-col md:grid md:grid-cols-[1fr_3fr_1fr_1fr_1fr_1fr] items-center border py-2 px-3 gap-2'>
+              <img src = {item.imgURL} alt = {item.name} className='w-24 h-24 object-cover' />
+              <p className='text-center md:text-start'>{item.name}</p>
+              <p>{item.brand}</p>
               <p className='flex items-center'>{currency} {item.price}</p>
               <div className='flex items-center gap-1'>
                 <span className="text-sm font-medium">{item.rating}</span>
                 <FiStar className="text-yellow-400" />                
               </div>
               <div className='flex justify-center gap-2'>
-                <button className='text-2xl text-green-500 hover:text-green-600 px-2 py-1 rounded' title='Edit'>
+                <button className='text-2xl text-green-500 hover:bg-gray-100 px-2 py-1 rounded' title='Edit'>
                   <FiEdit />
                 </button>
-                <button onClick={() => removeProduct(item._id)} className='text-2xl text-red-500 hover:text-red-600 px-2 py-1 rounded' title='Delete'>
+                <button onClick={() => removeProduct(item._id)} className='text-2xl text-red-500 hover:bg-gray-100 px-2 py-1 rounded' title='Delete'>
                   <FiTrash2 />
                 </button>
               </div>
