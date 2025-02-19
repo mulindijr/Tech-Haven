135373ed004038479ce183a0b586c3b72006d9f8
Brian <mulindijrn@gmail.com>
2025-02-19 16:53:19 +0300

    Delete the hr element

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -27,7 +27,6 @@ function App() {
       :
         <>
           <Navbar setToken = {setToken}/>
-          <hr />
           <div className="flex w-full">
             <Sidebar />
             <div className="w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base">
