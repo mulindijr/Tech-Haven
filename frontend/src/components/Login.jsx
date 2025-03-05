8557e4a8960efc4bdfd0a20fd1448b2244df811d
Brian <mulindijrn@gmail.com>
2025-03-05 03:49:21 +0300

    Refactor Login code to keep it clean

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -42,12 +42,9 @@ const Login = ({setShowLogin}) => {
 
   // Prevent scrolling when the modal is open
   useEffect(() => {
-    if (typeof setShowLogin === "function") {
-      document.body.style.overflow = "hidden"; // Prevent scrolling
-    } else {
-      document.body.style.overflow = "unset"; // Restore scrolling
-    }
-
+    if (setShowLogin) {
+      document.body.style.overflow = "hidden";
+    } 
     // Cleanup function to restore scrolling on unmount
     return () => {
       document.body.style.overflow = "unset";
@@ -55,14 +52,14 @@ const Login = ({setShowLogin}) => {
   }, [setShowLogin]);
 
   return (
-    <div className="absolute z-[1] w-full h-full bg-[#00000090] grid">
+    <div className="absolute z-[9999]  w-full h-full bg-[#00000090] grid">
       <div className="place-self-center animate-fadeIn">
         <form onSubmit={onSubmitHandler} className="flex flex-col w-full sm:max-w-96 m-auto mt-14 gap-4 text-gray-800 bg-slate-200 px-5 py-5 rounded mb-10">
           <div className="flex items-center justify-between mb-2">
             <h2 className="text-3xl">{currentState}</h2>
             <MdClose onClick={() => typeof setShowLogin === "function" && setShowLogin(false)} className="w-8 h-8 cursor-pointer"/>
           </div>      
-          {currentState === "Login" ? <></>: <input onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder="Fullname" required className="w-full px-3 py-2 border border-gray-400 outline-none rounded"/>}
+          {currentState !== 'Login' && <input onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder="Fullname" required className="w-full px-3 py-2 border border-gray-400 outline-none rounded"/>}
           <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" placeholder="Email" required className="w-full px-3 py-2 border border-gray-400 outline-none rounded"/>
           <div className="relative">
             <input onChange={(e) => setPassword(e.target.value)} value={password} type={showPassword ? "text" : "password"} placeholder="Password" required className="w-full px-3 py-2 border border-gray-400 outline-none rounded"/>
@@ -74,7 +71,7 @@ const Login = ({setShowLogin}) => {
 
           {currentState === 'Sign Up' 
           ? <div className="flex gap-2">
-              <input type="checkbox" required />
+              <input type="checkbox" required className="cursor-pointer"/>
               <p>I agree to terms of use and privacy policy</p>
             </div>
           : ""
