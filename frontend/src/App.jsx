912ad6c35f9396fd6522a21ec9d3bd269f785fb0
Brian <mulindijrn@gmail.com>
2025-03-06 23:50:46 +0300

    Refactor App component for improved structure and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home, Shop, About, Contact, Cart, Faqs, Product, PlaceOrder, Orders } from './pages';
+import { Home, Shop, About, Contact, Cart, Faqs, Product, PlaceOrder, Orders, Login } from './pages';
 import Navigation from './components/Navigation'; 
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar';
-import Login from './pages/Login';
-import { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const [showLogin, setShowLogin] = useState(false)
   return (  
     <>
-      {showLogin ? <Login setShowLogin = {setShowLogin}/> : <></>}  
       <ToastContainer />
       <div>
-        <Navigation setShowLogin = {setShowLogin}/>
+        <Navigation />
         <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>      
           <SearchBar />
           <Routes>
@@ -28,6 +24,7 @@ const App = () => {
             <Route path="/faqs" element={<Faqs />} />
             <Route path="/place-order" element={<PlaceOrder />} />
             <Route path="/orders" element={<Orders />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
           <Footer />
         </div>
