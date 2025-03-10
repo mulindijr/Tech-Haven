b7eaf607aaff383859d6df5725dcc723bc4c49ce
Brian <mulindijrn@gmail.com>
2025-03-10 12:14:46 +0300

    Set up routing with MainLayout and AuthLayout in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,34 @@
-import { Routes, Route } from 'react-router-dom';
-import { Home, Shop, About, Contact, Cart, Faqs, Product, PlaceOrder, Orders, Login } from './pages';
-import Navigation from './components/Navigation'; 
-import Footer from './components/Footer'
-import SearchBar from './components/SearchBar';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { Routes, Route } from "react-router-dom";
+import { Home, Shop, About, Contact, Cart, Faqs, Product, PlaceOrder, Orders, Login } from "./pages";
+import MainLayout from "./layouts/MainLayout";
+import AuthLayout from "./layouts/AuthLayout";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  return (  
+  return (
     <>
       <ToastContainer />
-      <div>
-        <Navigation />
-        <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>      
-          <SearchBar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/shop" element={<Shop />} />
-            <Route path="/product/:productId" element={<Product />} />
-            <Route path="/about-us" element={<About />} />
-            <Route path="/contact-us" element={<Contact />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/faqs" element={<Faqs />} />
-            <Route path="/place-order" element={<PlaceOrder />} />
-            <Route path="/orders" element={<Orders />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-          <Footer />
-        </div>
-      </div> 
-    </>  
+      <Routes>
+        {/* Routes that use MainLayout */}
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/about-us" element={<About />} />
+          <Route path="/contact-us" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/faqs" element={<Faqs />} />
+          <Route path="/place-order" element={<PlaceOrder />} />
+          <Route path="/orders" element={<Orders />} />
+        </Route>
+
+        {/* Routes that use AuthLayout */}
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
