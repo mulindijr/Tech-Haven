47ea3a0e87e371c617ca2251a1295a956c50a389
Brian <mulindijrn@gmail.com>
2025-03-11 17:45:12 +0300

    Add user authentication middleware to cart routes

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { addToCart, getCart, updateCart } from '../controllers/cartController.js';
+import userAuth from '../middleware/userAuth.js';
 
 const cartRouter = express.Router();
 
-cartRouter.post('/get', getCart)
-cartRouter.post('/add', addToCart)
-cartRouter.post('/update', updateCart)
+cartRouter.post('/get', userAuth, getCart)
+cartRouter.post('/add', userAuth, addToCart)
+cartRouter.post('/update', userAuth, updateCart)
 
 export default cartRouter;
