12b73643d71fe2537b06b06177d002dfe0eca5af
Brian <mulindijrn@gmail.com>
2025-03-11 22:38:57 +0300

    feat: Implement addToCart functionality

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,8 +1,30 @@
+import userModel from "../models/userModel";
 
 
 // Add Products To User Cart
 const addToCart = async (req, res)=> {
-    
+
+    try {
+
+        const { userId, itemId } = req.body;
+
+        const userData = await userModel.findById(userId);
+        let cartData = userData.cartData || {};
+
+        if (cartData[itemId]) {
+            cartData[itemId] += 1
+        } else {
+            cartData[itemId] =1
+        }
+
+        await userModel.findByIdAndUpdate(userId, { cartData });
+
+        res.status(200).json({message: "Product Added To Cart"});
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: error.message || "Internal Server Error"});        
+    }   
 }
 
 // Update User Cart
