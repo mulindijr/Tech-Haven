087377ec5ff9de5ca578c41e35148fc3f32db70b
Brian <mulindijrn@gmail.com>
2025-03-11 17:10:04 +0300

    Add Cart Controller Functions
    - Added  function to handle adding products to the user's cart.
    - Added  function to modify cart items.
    - Added  function to retrieve user cart data.
    - Exported all functions for use in routes.

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,18 @@
+
+
+// Add Products To User Cart
+const addToCart = async (req, res)=> {
+    
+}
+
+// Update User Cart
+const updateCart = async (req, res)=> {
+    
+}
+
+// Get User Cart Data
+const getCart = async (req, res)=> {
+    
+}
+
+export { addToCart, updateCart, getCart }
