0dc5949ae5721791c388961a854b5200a554d010
Brian <mulindijrn@gmail.com>
2025-03-12 23:21:56 +0300

    Add Get Cart API Functionality

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -54,7 +54,24 @@ const updateCart = async (req, res)=> {
 
 // Get User Cart Data
 const getCart = async (req, res)=> {
-    
+
+    try {
+
+        const { userId } = req.body;
+
+        const userData = await userModel.findById(userId);
+        if (!userData) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        
+        let cartData = userData.cartData || {};
+
+        res.status(200).json({cartData});
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: error.message || "Internal Server Error"}); 
+    }    
 }
 
 export { addToCart, updateCart, getCart }
