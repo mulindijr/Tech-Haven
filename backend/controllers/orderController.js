2e91cb11393839b9b2977ae8931628d4752bcbca
Brian <mulindijrn@gmail.com>
2025-03-18 10:56:57 +0300

    feat: add order controller functions for multiple payment methods and order management

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,38 @@
+
+
+// Placing orders using Cash On Delivery method
+const placeOrder = async (req, res) => {
+
+}
+
+// Placing orders using Stripe method
+const placeOrderStripe = async (req, res) => {
+
+}
+
+// Placing orders using Razorpay method
+const placeOrderRazorpay = async (req, res) => {
+
+}
+
+// Placing orders using Mpesa method
+const placeOrderMpesa = async (req, res) => {
+
+}
+
+// All Orders for Admin Panel
+const allOrders = async (req, res) => {
+    
+}
+
+//User Order Data for frontend
+const userOrders = async (req, res) => {
+    
+}
+
+// Update Order Status from admin panel
+const updateOrderStatus = async (req, res) => {
+    
+}
+
+export { placeOrder, placeOrderStripe, placeOrderRazorpay, placeOrderMpesa, allOrders, userOrders, updateOrderStatus }
