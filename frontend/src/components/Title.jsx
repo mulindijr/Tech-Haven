ad3ff91d96b2fd4b7a61c4e2bb2d5ff53ed4214e
Brian <mulindijrn@gmail.com>
2025-03-20 12:43:14 +0300

    Refactor: Reduce text size of the title component on small devices and above

--- frontend/src/components/Title.jsx
+++ frontend/src/components/Title.jsx
@@ -2,8 +2,8 @@ const Title = ({title1, title2}) => {
 
   return (
     <div className="inline-flex gap-2 items-center mb-3">
-        <p className="text-md sm:text-3xl text-gray-500 text-center">{title1}</p>
-        <span className="text-md sm:text-3xl text-red-400 text-center">{title2}</span>
+        <p className="text-md sm:text-2xl text-gray-500 text-center">{title1}</p>
+        <span className="text-md sm:text-2xl text-red-400 text-center">{title2}</span>
     </div>
   )
 }
