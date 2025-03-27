a67c0e07f1cef95899a410862e3b09f08612878b
Brian <mulindijrn@gmail.com>
2025-03-27 17:26:57 +0300

    feat: Implement getting all orders feature for the admin panel

---
+++ admin/src/pages/Orders.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { backendUrl, currency} from '../App'
+import { toast } from 'react-toastify'
+import { BiPackage } from "react-icons/bi";
+
+const Orders = ({token}) => {
+
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+
+    if (!token) {
+      return null;      
+    }
+
+    try {
+
+      const response = await axios.post(backendUrl + '/api/order/list', {}, { headers: {token} })
+
+      if (response.data.success) {
+        setOrders(response.data.orders)
+      }else{
+        toast.error(response.data.message)
+      }
+      
+    } catch (error) {
+      toast.error(error.message)      
+    }
+
+  }
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, [token])
+
+  return (
+    <div>
+      <h1>Orders</h1>
+      <div>
+        {
+          orders.map((order, index) => (
+            <div className='grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700' key={index}>
+              <BiPackage size={30} color="gray" className='w-12'/>
+              <div>
+                <div>
+                  {
+                    order.items.map((item, index) => {
+                      if (index===order.items.length - 1) {
+                        return <p className='py-0.5' key={index}> {item.name} X {item.quantity} </p>
+                      } else {
+                        return <p className='py-0.5' key={index}> {item.name} X {item.quantity}, </p>
+                      }
+                    })
+                  }
+                </div>
+                <p className='mt-3 mb-2 font-medium'>{order.address.firstName + " " + order.address.lastName}</p>
+                <div>
+                  <p> {order.address.street + ","} </p>
+                  <p> {order.address.city + ", " + order.address.state + ", " + order.address.country + ", " + order.address.zipcode} </p>
+                </div>
+                <p> {order.address.phone} </p>
+                <div>
+                  <p className='text-sm sm:text-[15px]'>Items : {order.items.length} </p>
+                  <p className='mt-3'>Method : {order.paymentMethod} </p>
+                  <p>Payment : {order.payment ? 'Done' : 'Pending'}  </p>
+                  <p>Date : {new Date(order.date).toLocaleString()} </p>
+                </div>
+                <p className='text-sm sm:text-[15px]'> {currency}{order.amount} </p>
+                <select value={order.status} className='p-2 font-semibold'>
+                  <option value="Order Placed"> Order Placed </option>
+                  <option value="Packing"> Packing </option>
+                  <option value="Shipped"> Shipped </option>
+                  <option value="Out for Delivery"> Out for Delivery </option>
+                  <option value="Delivered"> Delivered </option>
+                </select>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Orders
