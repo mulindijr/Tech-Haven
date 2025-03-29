c4b41dd9dd78f76bd0a719408cf91aa04bb76b8c
Brian <mulindijrn@gmail.com>
2025-03-29 18:54:53 +0300

    Refactor: Fix the admin orders frontend style

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -19,7 +19,7 @@ const Orders = ({token}) => {
       const response = await axios.post(backendUrl + '/api/order/list', {}, { headers: {token} })
 
       if (response.data.success) {
-        setOrders(response.data.orders)
+        setOrders(response.data.orders.reverse());
       }else{
         toast.error(response.data.message)
       }
@@ -56,7 +56,7 @@ const Orders = ({token}) => {
       <div>
         {
           orders.map((order, index) => (
-            <div className='grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700' key={index}>
+            <div className='grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700 ' key={index}>
               <BiPackage size={30} color="gray" className='w-12'/>
               <div>
                 <div>
@@ -76,21 +76,21 @@ const Orders = ({token}) => {
                   <p> {order.address.city + ", " + order.address.state + ", " + order.address.country + ", " + order.address.zipcode} </p>
                 </div>
                 <p> {order.address.phone} </p>
-                <div>
-                  <p className='text-sm sm:text-[15px]'>Items : {order.items.length} </p>
-                  <p className='mt-3'>Method : {order.paymentMethod} </p>
-                  <p>Payment : {order.payment ? 'Done' : 'Pending'}  </p>
-                  <p>Date : {new Date(order.date).toLocaleString()} </p>
-                </div>
-                <p className='text-sm sm:text-[15px]'> {currency}{order.amount} </p>
-                <select onChange={(event) => statusHandler(event, order._id)} value={order.status} className='p-2 font-semibold'>
-                  <option value="Order Placed"> Order Placed </option>
-                  <option value="Packing"> Packing </option>
-                  <option value="Shipped"> Shipped </option>
-                  <option value="Out for Delivery"> Out for Delivery </option>
-                  <option value="Delivered"> Delivered </option>
-                </select>
               </div>
+              <div>
+                <p className='text-sm sm:text-[15px]'>Items : {order.items.length} </p>
+                <p className='mt-3'>Method : {order.paymentMethod} </p>
+                <p>Payment : {order.payment ? 'Done' : 'Pending'}  </p>
+                <p>Date : {new Date (order.dateOrdered).toLocaleDateString()} </p>
+              </div>
+              <p className='text-sm sm:text-[15px]'> {currency} {order.amount} </p>
+              <select className='p-2 font-semibold'>
+                <option value="Order Placed"> Order Placed </option>
+                <option value="Packing"> Packing </option>
+                <option value="Shipped"> Shipped </option>
+                <option value="Out for Delivery"> Out for Delivery </option>
+                <option value="Delivered"> Delivered </option>
+              </select>              
             </div>
           ))
         }
