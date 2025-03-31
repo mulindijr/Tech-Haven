675a013dcf364cb4b7576b3c47ea20ce7525c289
Brian <mulindijrn@gmail.com>
2025-03-31 09:40:14 +0300

    Implement order placement functionality with Stripe integration

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,13 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
+import Stripe from "stripe";
+
+// Global variables
+const currency = 'kes'
+const deliveryCharge = 200
+
+// Gateway Initialize
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 // Placing orders using Cash On Delivery method
 const placeOrder = async (req, res) => {
@@ -34,6 +42,60 @@ const placeOrder = async (req, res) => {
 
 // Placing orders using Stripe method
 const placeOrderStripe = async (req, res) => {
+
+    try {
+
+        const { userId, items, amount, address } = req.body;
+        const { origin } = req.headers;
+
+        const orderData = {
+            userId,
+            items,
+            amount,
+            address,
+            paymentMethod: 'Stripe',
+            payment: false,
+            dateOrdered: Date.now()
+        }
+
+        const newOrder = new orderModel(orderData);
+        await newOrder.save();
+
+        const line_items = items.map((item) => ({
+            price_data: {
+                currency: currency,
+                product_data: {
+                    name: item.name
+                },
+                unit_amount: item.price * 100
+            },
+            quantity: item.quantity
+        }))
+
+        line_items.push({
+            price_data: {
+                currency: currency,
+                product_data: {
+                    name: 'Delivery Charges'
+                },
+                unit_amount: deliveryCharge * 100
+            },
+            quantity: 1
+        })
+
+        const session = await stripe.checkout.sessions.create({
+            success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+            line_items,
+            mode: 'payment',
+        })
+
+        res.status(201).json({success: true, session_url: session.url});
+
+    } catch (error) {
+        console.error("Error:", error)
+        res.status(500).json({success:false, message: error.message || "Internal Server Error" })
+    }
 
 }
 
