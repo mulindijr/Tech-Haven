0b8c26d0abda1708333fbbde5a05c93f9c967e88
Brian <mulindijrn@gmail.com>
2025-04-09 20:14:20 +0300

    refactor(user model): update shipping address to single object

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,24 +1,28 @@
 import mongoose from 'mongoose';
 
-const addressSchema = new mongoose.Schema({
-  street: { type: String, required: true },
-  city: { type: String, required: true },
-  state: { type: String, required: true },
-  zipcode: { type: String, required: true },
-  country: { type: String, required: true },
-  phone: { type: String, required: true },
-  isDefault: { type: Boolean, default: false },
-}, { _id: true });
+const addressSchema = new mongoose.Schema(
+  {
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zipcode: { type: String, required: true },
+    country: { type: String, required: true },
+    phone: { type: String, required: true },
+    isDefault: { type: Boolean, default: false },
+  }, { _id: false }
+);
 
-const userSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  cartData: { type: Object, default: {} },
-  profilePic: { type: String, default: "" },
-  addresses: [addressSchema],
-}, { minimize: false, timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    cartData: { type: Object, default: {} },
+    profilePic: { type: String, default: "" },
+    address: { type: addressSchema, required: false, default: {}, },
+  }, { minimize: false, timestamps: true }
+);
 
 const userModel = mongoose.models.User || mongoose.model('User', userSchema);
 
