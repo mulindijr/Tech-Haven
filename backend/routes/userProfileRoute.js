cdac3c232403c87df5f06283e762307fe59c73a2
Brian <mulindijrn@gmail.com>
2025-04-13 15:37:06 +0300

    Add multer middleware to handle profile picture upload in profile update route

--- backend/routes/userProfileRoute.js
+++ backend/routes/userProfileRoute.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { getUserProfile, updatePassword, updateUserProfile } from "../controllers/userProfileController.js"
 import userAuth from "../middleware/userAuth.js"
+import upload from "../middleware/multer.js";
 
 const profileRouter = express.Router();
 
 profileRouter.post("/get", userAuth, getUserProfile)
-profileRouter.post("/update", userAuth, updateUserProfile)
+profileRouter.post("/update", userAuth, upload.single("profilePic"), updateUserProfile)
 profileRouter.post("/password", userAuth, updatePassword)
 
 export default profileRouter;
