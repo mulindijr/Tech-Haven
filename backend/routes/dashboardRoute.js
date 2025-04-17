5fa5bc54074b3afde6851b0cc14996e9510fa321
Brian <mulindijrn@gmail.com>
2025-04-17 23:07:41 +0300

    Delete dashboard controller and router

--- backend/routes/dashboardRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getDashboardStats } from '../controllers/dashboardStatsController.js';
-
-const dashboardRouter = express.Router();
-
-dashboardRouter.post('/stats', getDashboardStats);
-
-export default dashboardRouter;
