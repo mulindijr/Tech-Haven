b4961191f98f3cf64fa546e54c8504bd9843e6c1
Brian <mulindijrn@gmail.com>
2025-05-01 17:02:43 +0300

    feat: add route for product details page using slug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
           <Route path="*" element={<Navigate to="/" />} />
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
-          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/product/:slug" element={<Product />} />
           <Route path="/about-us" element={<About />} />
           <Route path="/contact-us" element={<Contact />} />
           <Route path="/cart" element={<Cart />} />
