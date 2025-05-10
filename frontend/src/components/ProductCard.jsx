c603d5b1f80dc561d73da489f734fc00ec4bb6bb
Brian <mulindijrn@gmail.com>
2025-05-10 14:03:05 +0300

    refactor: move add to cart button outside of product link for better UX

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -2,32 +2,37 @@ import { useContext } from "react";
 import { renderStars } from "./utils";
 import { ShopContext } from "../context/ShopContext";
 import { Link } from "react-router-dom";
+import { AiOutlineShopping } from "react-icons/ai";
 
 const ProductCard = ({img, brand, name, price, rating, onClick, slug}) => {
 
   const {currency} =useContext(ShopContext)
 
-    return (
-      <Link 
-        className="border shadow-md rounded-md p-5 cursor-pointer group"
+  return (
+    <div className="border shadow-md rounded-md p-5 cursor-pointer group flex flex-col justify-between h-full">
+      <Link
         to={`/product/${slug}`}
         onClick={() => onClick(slug)}
       >
-      <div className="">
-        <div className="flex justify-center items-center">
-          <img
-            src={img}
-            alt={name}
-            className="transform transition-transform duration-500 group-hover:scale-105"
-          />
-        </div>
-        <span className="text-sm text-green-400">{brand}</span>
-        <p className="mt-2 text-sm leading-normal font-semibold line-clamp-2">{name}</p>
-        <div>{renderStars(rating)}</div>
+        <div>
+          <div className="flex justify-center items-center">
+            <img
+              src={img}
+              alt={name}
+              className="transform transition-transform duration-500 group-hover:scale-105"
+            />
+          </div>
+          <span className="text-sm text-green-400">{brand}</span>
+          <p className="mt-2 text-sm leading-normal font-semibold line-clamp-2">{name}</p>
+          <div>{renderStars(rating)}</div>
+        </div>          
+      </Link>
+      <div className="flex items-center justify-between">
         <p className="text-red-500 font-extrabold">{currency} {price}</p>
+        <AiOutlineShopping className="text-lg"/>
       </div>
-    </Link>
-    )
-  }
+    </div>
+  )
+}
   
 export default ProductCard;
